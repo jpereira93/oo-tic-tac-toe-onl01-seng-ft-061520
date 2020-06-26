e3ec8058ffafbbb92b7cb0cd2666dc3ba53493b1
Jessica Pereira <jcpereira070@gmail.com>
2020-06-26 19:46:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,28 +1,36 @@
 class TicTacToe
-  array = [9, " "]
-def initialize(board = nil)
-  @board = board || Array.new(9, " ")
+array = [ ]
+  def initialize(board = nil)
+    @board = board || Array.new(9, " ")
+  end
+WIN_COMBINATIONS =
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[6,4,2]
+def display_board
+   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+   puts "-----------"
+   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+   puts "-----------"
+   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+ end
+ def input_to_index(player1)
+   player1.to_i - 1
+ end
+ def move(position, player1 ="X")
+   @board[position]= player1
+ end
+def position_taken?(index_i)
+  @board[index_i]== "O"|| @board[index_i] == "X"
 end
-WIN_COMBINATIONS = [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0,3,6],
-  [1,4,7],
-  [2,5,8],
-  [0,4,8],
-  [6,4,2]
-
-  def display_board
-     puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-     puts "-----------"
-     puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-     puts "-----------"
-     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
-   end
-
-def input_to_index(player_one)
+def valid_move?(index_i)
+index_i.between?(0,8) && !position_taken?(index_i)
 end
-
-
-
+def turn_count
+end 
 end
